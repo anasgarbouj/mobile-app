9e82a85737c8a96194bb472c528427379018bb0e
UserThree <userthree@xtendplex.com>
2023-07-31 16:08:05 +0100

    init

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'xtendtv_virtual_ticket_application',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
