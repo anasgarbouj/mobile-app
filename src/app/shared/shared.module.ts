5353fff0193966779b8e4b89926771cecf78bb24
UserThree <userthree@xtendplex.com>
2023-08-16 15:43:07 +0100

    +proximity popup - changed divider into component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,22 +3,31 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { HeaderComponent } from './components/header/header.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { ProximityPopupComponent } from './components/proximity-popup/proximity-popup.component';
+import { DividerComponent } from './components/divider/divider.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    SearchBarComponent
+    SearchBarComponent,
+    ProximityPopupComponent,
+    DividerComponent
   ],
   imports: [
     CommonModule,
-    IonicModule.forRoot()
+    IonicModule.forRoot({
+      innerHTMLTemplatesEnabled: true
+    })
   ],
   exports: [
     IonicModule,
     HeaderComponent,
-    SearchBarComponent
+    SearchBarComponent,
+    ProximityPopupComponent,
+    DividerComponent,
+
   ]
 })
 export class SharedModule { }
