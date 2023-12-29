324b1e26cf81a68cca5bd0b5bedacb9c084d18a5
Nostalgia <userthree@xtendplex.com>
2023-12-29 17:19:42 +0100

    Updating plateform type

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,8 +12,10 @@ export class AppComponent implements OnInit {
   constructor(private platform: Platform, private router: Router) { }
 
   ngOnInit(): void {
-    if (!this.platform.is('mobileweb')) {
+    if (!this.platform.is('mobile')) {
       this.router.navigate(['/access-denied']);
+      console.log("Cannot open app in this device...");
+      
     }
   }
 }
