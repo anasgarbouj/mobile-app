7fe65378315fb73fb7660b561a64fb3f40f73adf
Nostalgia <userthree@xtendplex.com>
2024-01-02 15:54:19 +0100

    generate component + global config

---
+++ src/app/core/components/warning-delete-ticket-popup/warning-delete-ticket-popup.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-delete-ticket-popup',
+  templateUrl: './warning-delete-ticket-popup.component.html',
+  styleUrls: ['./warning-delete-ticket-popup.component.css'],
+})
+export class WarningDeleteTicketPopupComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
