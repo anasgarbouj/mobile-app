77bf75fc91faa55c912d6d506b24ae0ebec920ad
Nostalgie <userthree@xtendplex.com>
2024-01-16 15:20:41 +0100

    adding ticket component to private module

---
+++ src/app/private/components/ticket/ticket.component.ts
@@ -0,0 +1,49 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ticket',
+  templateUrl: './ticket.component.html',
+  styleUrls: ['./ticket.component.css'],
+})
+export class TicketComponent  implements OnInit {
+
+  
+  @Input()
+  isTicketCalled : boolean = false
+
+  @Input()
+  toDelete : boolean = false
+
+
+  @Input()
+  ticketTitle : String = "This is the default ticket title"
+
+  @Input()
+  ticketNumber : String ="056"
+
+  @Input()
+  ticketServicePrefix : String ="p"
+
+  @Input()
+  ticketService: String ="prise de sang"
+
+  @Input()
+  ticketRoom : String  = "S1"
+
+  @Input()
+  waitingTime : String ="0"
+
+  @Input()
+  date : String ="12/06/2022"
+
+  @Input()
+  time : String ="15:20"
+
+  @Input()
+  website : String ="https://www.siteweb.fr/"
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
