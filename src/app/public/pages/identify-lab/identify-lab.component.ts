57f084be2ac9a78f1f1e0bd8165332abf3b09605
Nostalgie <userthree@xtendplex.com>
2024-01-31 11:58:54 +0100

    qr code scan : request camera permission

--- src/app/public/pages/identify-lab/identify-lab.component.ts
+++ src/app/public/pages/identify-lab/identify-lab.component.ts
@@ -14,4 +14,37 @@ export class IdentifyLabComponent {
   navigateToLabs() {
     this._router.navigate(["/labs"])
   }
+
+  
+  async startQrScan(){
+    console.log("qr code clicked...");
+    const cameraPermissionState = await this.checkAndRequestCameraPermission()
+    
+    if(!cameraPermissionState){
+      console.log("Exit Qr Scan ..No camera permission was granted...");
+      return;
+    }
+    console.log("Starting Qr Code Scan...");
+
+  }
+
+   async checkAndRequestCameraPermission() : Promise<boolean>{
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      console.log('Camera permission already granted:', stream);
+      return true;
+  
+    } catch (error) {
+      console.warn('Camera permission not granted. Requesting permission...');
+      try {
+        await navigator.mediaDevices.getUserMedia({ video: true });
+        console.log('Camera permission granted after requesting.');
+        return true;
+
+      } catch (error) {
+        console.error('Failed to get camera permission:', error);
+        return false;
+      }
+    }
+  }
 }
